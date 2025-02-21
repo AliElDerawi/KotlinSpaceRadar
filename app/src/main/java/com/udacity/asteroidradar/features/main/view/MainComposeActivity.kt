39bd1b6474f76535c1ce8c279ed50348e6d81f8b
Ali Al-Derawi <aalderawi2013@students.iugaza.edu.ps>
2025-02-21 15:17:16 +0200

    refactor: Apply navigation bars padding to Surface in MainComposeActivity
    The change applies padding from the navigation bars to the `Surface` composable to ensure content is displayed correctly.

--- app/src/main/java/com/udacity/asteroidradar/features/main/view/MainComposeActivity.kt
+++ app/src/main/java/com/udacity/asteroidradar/features/main/view/MainComposeActivity.kt
@@ -2,7 +2,11 @@ package com.udacity.asteroidradar.features.main.view
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -17,7 +21,7 @@ class MainComposeActivity : ComponentActivity() {
         setContent {
             AsteroidRadarTheme() {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize().padding(WindowInsets.navigationBars.asPaddingValues()),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     AsteroidApp()
