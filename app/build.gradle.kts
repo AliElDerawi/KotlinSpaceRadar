a88ed259c3b8a8a5824314cff3aaaf0caa51a153
AliElDerawi <aalderawi2013@students.iugaza.edu.ps>
2026-02-14 10:15:43 +0200

    chore: Move secrets block in build.gradle.kts
    This commit moves the `secrets` configuration block outside of the `android` block in the `app/build.gradle.kts` file. This change corrects its placement to the top level of the build script, where it is expected to be declared.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,21 +44,21 @@ android {
     buildFeatures {
         buildConfig = true
     }
+}
 
-    secrets {
-        // Optionally specify a different file name containing your secrets.
-        // The plugin defaults to "local.properties"
-        propertiesFileName = "secrets.properties"
+secrets {
+    // Optionally specify a different file name containing your secrets.
+    // The plugin defaults to "local.properties"
+    propertiesFileName = "secrets.properties"
 
-        // A properties file containing default secret values. This file can be
-        // checked in version control.
-        defaultPropertiesFileName = "local.defaults.properties"
+    // A properties file containing default secret values. This file can be
+    // checked in version control.
+    defaultPropertiesFileName = "local.defaults.properties"
 
-        // Configure which keys should be ignored by the plugin by providing regular expressions.
-        // "sdk.dir" is ignored by default.
-        ignoreList.add("keyToIgnore") // Ignore the key "keyToIgnore"
-        ignoreList.add("sdk.*")       // Ignore all keys matching the regexp "sdk.*"
-    }
+    // Configure which keys should be ignored by the plugin by providing regular expressions.
+    // "sdk.dir" is ignored by default.
+    ignoreList.add("keyToIgnore") // Ignore the key "keyToIgnore"
+    ignoreList.add("sdk.*")       // Ignore all keys matching the regexp "sdk.*"
 }
 
 kotlin {
